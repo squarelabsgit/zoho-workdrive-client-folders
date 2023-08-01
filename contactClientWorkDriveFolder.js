76447555a7b197bcae476c2143fed7a876cedd92
Square Labs <116999254+squarelabsgit@users.noreply.github.com>
2023-08-01 15:27:13 +0930

    Create contactClientWorkDriveFolder.js

---
+++ contactClientWorkDriveFolder.js
@@ -0,0 +1,31 @@
+//Required Variables
+clientFolderUrl = "<<ENTER YOUR CLIENT FOLDER URL HERE>>";
+clientFolderId = clientFolderUrl.getSuffix("/ws/").getPrefix("/folders/files");
+clientFolderBaseUrl = clientFolderUrl.getPrefix("/files");
+//Get Contact Record
+contactMap = zoho.crm.getRecordById("Contacts",contactId);
+//check if workdrive folder already exists
+if(isnull(contactMap.get("WorkDrive_Folder_ID")))
+{
+	//Create Contact Folder
+	folderName = contactMap.get("Client_Number") + " - " + contactMap.get("Full_Name");
+	createContactWorkdriveFolder = zoho.workdrive.createFolder(folderName,clientFolderId,"workdrive_connection");
+	//Get the new contact folder ID
+	contactFolderId = createContactWorkdriveFolder.get("data").get("id");
+	//Prepare Update Map
+	updateMap = Map();
+	updateMap.put("WorkDrive_Folder_ID",contactFolderId);
+	//Update Contact Record With Workdrive Folder ID
+	updateResponse = zoho.crm.updateRecord("Contacts",contactId,updateMap);
+	//Open workdrive folder in a new Tab
+	openUrl(clientFolderBaseUrl + "/" + contactFolderId,"same window");
+	//info contactFolderId so it can be returned to the Create Deal Workdrive Function
+	info contactFolderId;
+	return "";
+}
+else
+{
+	//Open workdrive folder in a new Tab
+	openUrl(clientFolderBaseUrl + "/" + contactMap.get("WorkDrive_Folder_ID"),"same window");
+	return "";
+}
